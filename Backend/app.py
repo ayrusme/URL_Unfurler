03ffc7a12604e80797999f10319854ee78d17df3
Surya Raman <ayrusme@gmail.com>
2017-11-27 11:54:51 +0530

    Backend implemented

--- Backend/app.py
+++ Backend/app.py
@@ -11,15 +11,39 @@ CORS(APP)
 @APP.route("/get-thumbnail", methods=["POST"])
 def thumbnail_generator():
     """Route Function to handle the thumbnail request from the frontend"""
-    request_json = request.get_json()
-
-    webpage, message = url_preview.send_request(request_json['webpage_url'])
+    website_url = request.json['url']
+    webpage, message = url_preview.send_request(website_url)
     if webpage is not None:
-        #Construct the soup object
-        soup = url_preview.get_soup_object(webpage)
-        
-    else:
-        return jsonify({"status":"error", "message": message})
+        try:
+            #Construct the soup object
+            soup_object = url_preview.get_soup_object(webpage)
+            #Get the title of the artcile
+            title = url_preview.get_title(soup_object)
+            #Get the website of the article
+            website_name = url_preview.get_url(soup_object)
+            if website_name is None:
+                website_name = website_url.split("//", 1)[1].split("/", 1)[0]
+
+            #Get the description of the article
+            description = url_preview.get_description(soup_object)
+
+            #Get the published date and time of the article
+            date_time = url_preview.get_date_time(website_url)
+
+            #Get the link to the preview image
+            image_url = url_preview.get_preview_image(soup_object)['content']
+
+            return jsonify({"status" : message,
+                "content" : {
+                    "title" : title,
+                    "website_name" : website_name,
+                    "description" : description,
+                    "date_time" : date_time,
+                    "preview_image" : image_url
+                    }
+                })
+        except Exception as exp:
+            return jsonify({"status": "error", "message": str(exp)}), 400
 
 if __name__ == "__main__":
     """"Main function to start the Flask server"""
--- Backend/url_preview.py
+++ Backend/url_preview.py
@@ -4,6 +4,7 @@ import sys
 
 import requests as rq
 from bs4 import BeautifulSoup
+from newspaper import Article
 
 def send_request(url):
     """Function to get the html page"""
@@ -18,12 +19,31 @@ def get_soup_object(resp):
     return BeautifulSoup(resp.content, "html.parser")
 
 def get_title(soup):
-    """Function to get the title of the html page"""
+    """Function to get the title from the soup object"""
     title = soup.find("meta", property="og:title")['content']
     if title == "" or title is None:
         title = soup.find("title")['content']
     return title
 
 def get_url(soup):
-    """Function to find the URL of the webpage"""
-    return soup.find("meta",  property="og:url")['content']
+    """Function to find the URL from the soup object"""
+    return soup.find("meta",  property="og:site_name")['content']
+
+def get_description(soup):
+    """Function to get the description from the soup object"""
+    description = soup.find('meta', property='og:description')['content'] 
+    if description is None or description == "":
+        description =  soup.find('meta', attrs={'name':'description'})['content']
+    return description
+
+def get_date_time(webpage_url):
+    """Function to get the date and time from the webpage"""
+    article = Article(webpage_url)
+    article.download()
+    article.parse()
+    return article.publish_date.strftime('%d/%m/%Y')
+
+def get_preview_image(soup):
+    """Function to get the description from the soup object"""
+    return soup.find('meta', property='og:image')
+
